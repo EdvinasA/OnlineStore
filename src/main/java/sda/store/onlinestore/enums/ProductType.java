ab4d3eaec0db500e13e5314fbc9787ecb6fc218e
Edvinas <edvinasalimas98@gmail.com>
2021-06-03 19:14:02 +0300

    Fix

--- src/main/java/sda/store/onlinestore/enums/ProductType.java
+++ src/main/java/sda/store/onlinestore/enums/ProductType.java
@@ -1,7 +1,24 @@
 package sda.store.onlinestore.enums;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public enum ProductType {
     COMPUTER,
     MONITOR,
-    OTHERS
+    OTHERS;
+
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
 }
