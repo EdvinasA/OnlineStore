b175ded7fa9d09952e86dfbfc825a377dda1c94a
Edvinas <edvinasalimas98@gmail.com>
2021-06-05 22:38:03 +0300

    Leaving spring security

--- src/main/java/sda/store/onlinestore/OnlineStoreApplication.java
+++ src/main/java/sda/store/onlinestore/OnlineStoreApplication.java
@@ -1,13 +1,18 @@
 package sda.store.onlinestore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
 
 @SpringBootApplication
 public class OnlineStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineStoreApplication.class, args);
+
     }
 
 }
--- src/main/java/sda/store/onlinestore/controller/UserController.java
+++ src/main/java/sda/store/onlinestore/controller/UserController.java
@@ -20,11 +20,11 @@ public class UserController {
                 user.getUserName().equals("user") && user.getPassword().equals("password");
     }
 
-    @RequestMapping("/user")
-    public Principal user(HttpServletRequest request) {
-        String authToken = request.getHeader("Authorization")
-                .substring("Basic".length()).trim();
-        return () ->  new String(Base64.getDecoder()
-                .decode(authToken)).split(":")[0];
-    }
+//    @RequestMapping("/user")
+//    public Principal user(HttpServletRequest request) {
+//        String authToken = request.getHeader("Authorization")
+//                .substring("Basic".length()).trim();
+//        return () ->  new String(Base64.getDecoder()
+//                .decode(authToken)).split(":")[0];
+//    }
 }
