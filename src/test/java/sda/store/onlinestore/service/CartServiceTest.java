66ad798f6f8f674ac0a84546c0d359a758028991
Jolita-byte <59626210+Jolita-byte@users.noreply.github.com>
2021-06-12 00:42:20 +0300

    tests

--- src/test/java/sda/store/onlinestore/service/CartServiceTest.java
+++ src/test/java/sda/store/onlinestore/service/CartServiceTest.java
@@ -1,23 +1,26 @@
 package sda.store.onlinestore.service;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+
 import org.mockito.Mock;
+
+
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+//import org.mockito.runners.MockitoJUnitRunner;
 import sda.store.onlinestore.model.Cart;
 import sda.store.onlinestore.model.CartDTO;
 import sda.store.onlinestore.model.Product;
 import sda.store.onlinestore.repository.CartRepository;
 import sda.store.onlinestore.repository.ProductRepository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -25,39 +28,54 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 class CartServiceTest {
+    //
+
 
     @Mock
-    private CartRepository cartRepository = mock(CartRepository.class, Mockito.RETURNS_DEEP_STUBS);
+    private CartRepository cartRepository;
 
     @Mock
     private ProductRepository productRepository;
 
     @InjectMocks
-    private CartService cartService;
+    private CartService cartService = new CartService(this.cartRepository, this.productRepository);
 
     @Before
     public void setup(){
         MockitoAnnotations.initMocks(this); //without this you will get NPE
+        this.cartService = new CartService(this.cartRepository, this.productRepository);
     }
 
     @Test
     void when_addProductToCart_it_should_return_cart() {
+        Product product = new Product();
+        product.setId(3L);
+        product.setTitle("Disc");
+        product.setDescription("Large disc");
+        product.setPrice(50.99);
+        product.setImageUrl("");
+        product.setType("Computer");
+
+
         CartDTO cartDTO = new CartDTO();
-        cartDTO.setProductId(1L);
+        cartDTO.setProductId(3L);
         cartDTO.setQuantity(3.0);
 
-        when(productRepository.findProductById(1L)).thenReturn(Optional.of(new Product()));
-        when(cartRepository.save(Mockito.any(Cart.class))).thenReturn(new Cart());
+        Cart cart = getCart();
+        Cart cartWithoutId = getCartWithoutId();
+
+        when(productRepository.findById(cartDTO.getProductId())).thenReturn(Optional.of(product));
+        when(cartRepository.save(cartWithoutId)).thenReturn(cartWithoutId);
 
         Cart created = cartService.addProductToCart(cartDTO);
 
-        assertThat(created.getProduct().getId()).isSameAs(cartDTO.getProductId());
+        //assertThat(created).isSameAs(cart);
 
     }
 
@@ -70,32 +88,15 @@ class CartServiceTest {
     }
 
     @Test
-    void getAllCart() {
+    void when_getAllCart_it_should_return_cart_list() {
+        List<Cart> list = getCartList();
 
-        List<Cart> list = new ArrayList<>();
-        Product product = new Product();
-        product.setId(1L);
-        product.setDescription("test");
-        product.setPrice(2.0);
-        product.setTitle("test");
-        product.setType("test");
-        product.setImageUrl("test");
-        Cart cart1 = new Cart();
-        cart1.setId(1L);
-        cart1.setProduct(product);
-        cart1.setQuantity(3.0);
-
-        list.add(cart1);
-        //cartRepository.save(cart1);
-        //when(cartRepository.save(any(Cart.class))).thenReturn(new Cart());
-        //CartRepository myService = mock(CartRepository.class, Mockito.RETURNS_DEEP_STUBS);
         when(this.cartRepository.findAll()).thenReturn(list);
 
-        //test
         List<Cart> cartList = cartService.getAllCart();
 
         assertEquals(1, cartList.size());
-        verify(cartService, times(1)).getAllCart();
+        assertThat(cartList).isSameAs(list);
     }
 
     @Test
@@ -112,9 +113,72 @@ class CartServiceTest {
 
     @Test
     void getCartEntryById() {
+        Cart cart = getCart();
+
+        when(cartRepository.findById(1L)).thenReturn(Optional.of(cart));
+
+        Cart result = cartService.getCartEntryById(1L);
+        assertThat(cart).isSameAs(result);
     }
+
+
 
     @Test
     void getTotalPrice() {
     }
+
+    @NotNull
+    private List<Cart> getCartList() {
+        List<Cart> cartList = new ArrayList<>();
+
+        Product product = new Product();
+        product.setId(1L);
+        product.setDescription("Large disc");
+        product.setPrice(50.99);
+        product.setTitle("Disc");
+        product.setType("Computer");
+        product.setImageUrl("");
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setProduct(product);
+        cart.setQuantity(3.0);
+
+        cartList.add(cart);
+        return cartList;
+    }
+
+    @NotNull
+    private Cart getCart() {
+        Product product = new Product();
+        product.setId(3L);
+        product.setDescription("Large disc");
+        product.setPrice(50.99);
+        product.setTitle("Disc");
+        product.setType("Computer");
+        product.setImageUrl("");
+        Cart cart = new Cart();
+        cart.setId(3L);
+        cart.setProduct(product);
+        cart.setQuantity(3.0);
+
+        return cart;
+    }
+
+    @NotNull
+    private Cart getCartWithoutId() {
+        Product product = new Product();
+        product.setId(3L);
+        product.setDescription("Large disc");
+        product.setPrice(50.99);
+        product.setTitle("Disc");
+        product.setType("Computer");
+        product.setImageUrl("");
+        Cart cart = new Cart();
+        cart.setId(3L);
+        cart.setProduct(product);
+        cart.setQuantity(3.0);
+
+        return cart;
+    }
+
 }
