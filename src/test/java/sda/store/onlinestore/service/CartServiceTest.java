f667591c74e9cf492f12975b8d67046f5a52c998
Edvinas <edvinasalimas98@gmail.com>
2021-06-11 19:33:14 +0300

    done tests for ProductService class

---
+++ src/test/java/sda/store/onlinestore/service/CartServiceTest.java
@@ -0,0 +1,44 @@
+package sda.store.onlinestore.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartServiceTest {
+
+    @Test
+    void addProductToCart() {
+    }
+
+    @Test
+    void checkIfProductExists() {
+    }
+
+    @Test
+    void deleteCartProductById() {
+    }
+
+    @Test
+    void getAllCart() {
+    }
+
+    @Test
+    void addProductQuantityInCart() {
+    }
+
+    @Test
+    void subtractProductQuantityInCart() {
+    }
+
+    @Test
+    void checkIfCartIsZero() {
+    }
+
+    @Test
+    void getCartEntryById() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+}
